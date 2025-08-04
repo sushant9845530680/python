6d0b14580c3c87e02735c113b1d822ff33290ef1
sushant9845530680 <sushantadhikari041@gmail.com>
2025-08-04 11:59:58 +0545

    comment

---
+++ 2.py
@@ -0,0 +1,42 @@
+a = 34
+b = 2
+#arithmetic operators
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a% b=",a%b)
+print("a*b=",a*b)
+print("a/b=",a/b)
+print("a//b=",a//b)
+print("a**b=",a**b)
+
+#Conditional Operators
+#print(a>4)
+#print(a<4)
+#print(a>=4)
+#print(a<=4)
+#print(a==34)
+#print(a=!4)
+
+
+#logical operators
+c= True
+d= False
+#print(c and c)
+#print(c and d)
+#print(c and c)
+#print(c and d)
+
+print(c or c)
+print (c or d)
+print(c or c)
+print(c or d)
+
+#not operator 
+print(not(True))
+print(not(False))
+
+#assignment operators
+a=32
+print(a)
+a+=3 #a*=3
+print(a)
