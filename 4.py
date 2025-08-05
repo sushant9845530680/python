9f28a90745c3fb93744203106bd9d114cbe915ce
sushant9845530680 <sushantadhikari041@gmail.com>
2025-08-05 11:05:35 +0545

    gsd

--- 4.py
+++ 4.py
@@ -3,3 +3,6 @@ print(random.randrange(1,10))
 x="hello"
 print(x)
 print(x[1])
+
+for y in "x":
+    print(x)
---
+++ loop.py
@@ -0,0 +1,15 @@
+for x in "banana":
+    print(x)
+y="       sushant adhikari            "
+print(len(y))
+print(y.upper())  #upercase
+print(y.strip())  #remove all blank space from back and front
+
+#check the word wheatherr present in the text or not
+txt="my name is sushant adhikari"
+if "sushant" in txt:   
+ print("yes, 'sushant' is in txt") 
+
+x="Sushant Adhikari"
+print(x[2:7]) #print 2 index to 7
+print(x[7:])
